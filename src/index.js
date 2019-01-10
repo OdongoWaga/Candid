6317587ea04de60f65ad0f05ebc52b5affaddbea
Waga Odongo <wagaodongo@gmail.com>
2019-01-10 21:48:51 +0300

    Redux
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,21 +3,48 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, withRouter} from 'react-router-dom'
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
-const Root =() => (
-<Router>
-    <Switch>
-        <Route exact path="/" component={App} />
-        <Route exact path="/login" component={Login} />
-        <Route  exact path="/register" component={Register} />
-    </Switch>
-</Router>
-)
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {composewithDevTools} from 'redux-devtools-extension';
+import rootReducer from "./reducers";
+import { setUser } from "./actions";
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools());
+
+class Root extends React.Component {
+    componentDidMount() {
+      firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+          this.props.history.push("/");
+        }
+      });
+    }
+  
+    render() {
+      return (
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      );
+    }
+  }
+  
+  const RootWithAuth = withRouter(Root);
+  
+  ReactDOM.render(
+      <Provider store ={store}>
+        <Router>
+      <RootWithAuth />
+    </Router>
+      </Provider>,
+    document.getElementById("root")
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
