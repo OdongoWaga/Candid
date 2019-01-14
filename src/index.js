a935909d5deaf377234dc60b53f88b7a675d7cd2
Waga Odongo <wagaodongo@gmail.com>
2019-01-14 21:36:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,52 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import firebase from "./firebase";
 
-import {BrowserRouter as Router, Switch, Route, withRouter} from 'react-router-dom'
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import firebase from "./firebase"
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import "semantic-ui-css/semantic.min.css";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
+
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
 import { setUser } from "./actions";
 
 const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
-    componentDidMount() {
-      firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-          this.props.history.push("/");
-        }
-      });
-    }
-  
-    render() {
-      return (
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-        </Switch>
-      );
-    }
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        // console.log(user);
+        this.props.setUser(user);
+        this.props.history.push("/");
+      }
+    });
   }
-  
-  const RootWithAuth = withRouter(Root);
-  
-  ReactDOM.render(
-      <Provider store ={store}>
-        <Router>
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const RootWithAuth = withRouter(
+  connect(
+    null,
+    { setUser }
+  )(Root)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
       <RootWithAuth />
     </Router>
-      </Provider>,
-    document.getElementById("root")
-  );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  </Provider>,
+  document.getElementById("root")
+);
